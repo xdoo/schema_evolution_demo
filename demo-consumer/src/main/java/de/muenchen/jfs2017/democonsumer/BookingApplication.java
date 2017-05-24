4e6acdd751d8687fba5abbc7bba91f2a37a3e960
xdoo <claus.straube@gmail.com>
2017-05-24 12:36:26 +0200

    schema registry client configuration

--- demo-consumer/src/main/java/de/muenchen/jfs2017/democonsumer/BookingApplication.java
+++ demo-consumer/src/main/java/de/muenchen/jfs2017/democonsumer/BookingApplication.java
@@ -3,9 +3,11 @@ package de.muenchen.jfs2017.democonsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.schema.client.EnableSchemaRegistryClient;
 
 @SpringBootApplication
 @EnableBinding(ServiceMessaging.class)
+@EnableSchemaRegistryClient
 public class BookingApplication {
 
 	public static void main(String[] args) {
