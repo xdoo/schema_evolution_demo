f49aebb424d4cad3ef70d5159b4391294dbabc77
xdoo <claus.straube@gmail.com>
2017-06-21 15:56:44 +0200

    avro test project

---
+++ avro-demo/src/main/java/com/example/avrodemo/AvroDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.avrodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroDemoApplication.class, args);
+	}
+}
