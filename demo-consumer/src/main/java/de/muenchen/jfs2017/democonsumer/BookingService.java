5bd472808abc9c28361263865ea0b5351d5ccd94
xdoo <claus.straube@gmail.com>
2017-06-30 11:17:04 +0200

    some changes for avro

--- demo-consumer/src/main/java/de/muenchen/jfs2017/democonsumer/BookingService.java
+++ demo-consumer/src/main/java/de/muenchen/jfs2017/democonsumer/BookingService.java
@@ -2,8 +2,7 @@ package de.muenchen.jfs2017.democonsumer;
 
 import com.google.common.collect.Lists;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.java.Log;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +11,24 @@ import org.springframework.stereotype.Service;
  * @author claus
  */
 @Service
+@Log
 public class BookingService {
-    
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
-    
+
     private final List<Employee> employees;
 
     public BookingService() {
         this.employees = Lists.newArrayList();
     }
-    
+
     @StreamListener(ServiceMessaging.HIRE_EMPLOYEE)
     public void addEmployee(Employee employee) {
-        log.info(String.format("adding employee %s %s to my list.", employee.getForename(), employee.getSurename()));
+        log.info(String.format("adding employee to my list: \n %s", employee.toString()));
         this.employees.add(employee);
     }
-    
+
+//    @StreamListener(ServiceMessaging.HIRE_EMPLOYEE)
+//    public void addEmployee(String msg) {
+//        log.info(msg);
+//    }
+
 }
--- demo-producer/src/main/java/de/muenchen/jfs2017/demoproducer/RegistrationService.java
+++ demo-producer/src/main/java/de/muenchen/jfs2017/demoproducer/RegistrationService.java
@@ -1,5 +1,6 @@
 package de.muenchen.jfs2017.demoproducer;
 
+import lombok.extern.java.Log;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
  * @author claus
  */
 @Service
+@Log
 public class RegistrationService {
     
     private final ServiceMessaging messaging;
@@ -17,6 +19,7 @@ public class RegistrationService {
     }
     
     public void hireEmployee(Employee employee) {
+        this.log.info(String.format("sending employee: \n%s", employee.toString()));
         this.messaging.hireEmployeeEvent().send(MessageBuilder.withPayload(employee).build());
     }
     
