9e044f79ac4c677c365fac52a4bf6e90cdd66973
xdoo <claus.straube@gmail.com>
2017-07-03 15:09:08 +0200

    cleansed registry server

--- schema-registry/src/main/java/de/muenchen/jfs2017/schemaregistry/SchemaRegistryApplication.java
+++ schema-registry/src/main/java/de/muenchen/jfs2017/schemaregistry/SchemaRegistryApplication.java
@@ -2,10 +2,7 @@ package de.muenchen.jfs2017.schemaregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.stream.schema.server.EnableSchemaRegistryServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableSchemaRegistryServer
@@ -13,10 +10,5 @@ public class SchemaRegistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchemaRegistryApplication.class, args);
-	}
-        
-        @Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
 	}
 }
