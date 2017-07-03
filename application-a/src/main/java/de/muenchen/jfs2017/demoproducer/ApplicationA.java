c00c829380b71dbb1f79310f3c0de282423db9cb
xdoo <claus.straube@gmail.com>
2017-07-03 15:15:09 +0200

    added annotation

--- application-a/src/main/java/de/muenchen/jfs2017/demoproducer/ApplicationA.java
+++ application-a/src/main/java/de/muenchen/jfs2017/demoproducer/ApplicationA.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.schema.avro.AvroSchemaMessageConverter;
+import org.springframework.cloud.stream.schema.client.EnableSchemaRegistryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.util.MimeType;
 
 @SpringBootApplication
 @EnableBinding(ServiceMessaging.class)
+@EnableSchemaRegistryClient
 public class ApplicationA {
 
     public static void main(String[] args) {
